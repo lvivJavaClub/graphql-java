15123a2f03cee4e199db7a1eb53d85a50003a151
Ihor Banadiga <banadiga@gmail.com>
2018-06-21 10:06:20 +0300

    hi

--- src/main/java/com/lohika/jclub/graphqljava/GraphQLJavaConfig.java
+++ src/main/java/com/lohika/jclub/graphqljava/GraphQLJavaConfig.java
@@ -1,7 +1,16 @@
 package com.lohika.jclub.graphqljava;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GraphQLJavaConfig {
+  @Bean
+  public GraphQLQueryResolver query() {
+    return new GraphQLQueryResolver() {
+      public String hi() { return "Hello,!"; }
+    };
+  }
 }
